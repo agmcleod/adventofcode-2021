4022064bac693e60b6f7c1bb0b2cc7f616c74efd
Aaron McLeod <aaron.mcleod@wealthsimple.com>
2022-10-21 14:55:03 -0400

    cheated a bit for p2, but i understand the method now. why track each number, when you can count how many of them there are. 0-8 means a 9len array

--- 6/src/main.rs
+++ 6/src/main.rs
@@ -2,32 +2,39 @@ use std::io::Result;
 
 use read_input::read_text;
 
+fn generate_pop(mut lantern_fish: [usize; 9], times: usize) {
+    for _ in 0..times {
+        lantern_fish =
+            lantern_fish
+                .iter()
+                .enumerate()
+                .fold([0; 9], |mut new_lantern_fish, (i, n)| {
+                    if i == 0 {
+                        new_lantern_fish[6] += n;
+                        new_lantern_fish[8] += n;
+                    } else {
+                        new_lantern_fish[i - 1] += n;
+                    }
+                    new_lantern_fish
+                });
+    }
+
+    println!("{}", lantern_fish.iter().sum::<usize>());
+}
+
 fn main() -> Result<()> {
     let text = read_text("6/input.txt")?;
 
-    let mut lantern_fish: Vec<i32> = text
+    let lantern_fish = text
         .split(",")
-        .map(|number| number.parse::<i32>().unwrap())
-        .collect();
+        .map(|number| number.parse::<usize>().unwrap())
+        .fold([0; 9], |mut lantern_fish, num| {
+            lantern_fish[num % 9] += 1;
+            lantern_fish
+        });
 
-    for _ in 0..256 {
-        let mut next_to_spawn = Vec::new();
-
-        for fish in &mut lantern_fish {
-            if *fish == 0 {
-                *fish = 6;
-                next_to_spawn.push(8);
-            } else {
-                *fish -= 1;
-            }
-        }
-
-        if next_to_spawn.len() > 0 {
-            lantern_fish.append(&mut next_to_spawn);
-        }
-    }
-
-    println!("{}", lantern_fish.len());
+    generate_pop(lantern_fish.clone(), 80);
+    generate_pop(lantern_fish, 256);
 
     Ok(())
 }
