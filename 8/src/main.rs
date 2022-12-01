e4efb6dbe8a967f6d9709bf3e28fd3390cc069b4
Aaron McLeod <aaron.mcleod@wealthsimple.com>
2022-12-01 16:04:56 -0500

    using segment maps

--- 8/src/main.rs
+++ 8/src/main.rs
@@ -1,22 +1,22 @@
+use std::collections::{HashMap, HashSet};
 use std::io::Result;
 
-use bitflags::bitflags;
 use read_input::read_text;
-
-bitflags! {
-    struct Flags: u16 {
-        const A = 1;
-        const B = 2;
-        const C = 4;
-        const D = 8;
-        const E = 16;
-        const F = 32;
-        const G = 64;
-    }
-}
 
 fn main() -> Result<()> {
     let text = read_text("8/input.txt")?;
+
+    let mut segment_display_map = HashMap::new();
+    segment_display_map.insert(0, HashSet::from(["a", "b", "c", "e", "f", "g"]));
+    segment_display_map.insert(1, HashSet::from(["c", "f"]));
+    segment_display_map.insert(2, HashSet::from(["a", "c", "d", "e", "g"]));
+    segment_display_map.insert(3, HashSet::from(["a", "c", "d", "f", "g"]));
+    segment_display_map.insert(4, HashSet::from(["b", "c", "d", "f"]));
+    segment_display_map.insert(5, HashSet::from(["a", "b", "d", "f", "g"]));
+    segment_display_map.insert(6, HashSet::from(["a", "b", "d", "e", "f", "g"]));
+    segment_display_map.insert(7, HashSet::from(["a", "c", "f"]));
+    segment_display_map.insert(8, HashSet::from(["a", "b", "c", "d", "e", "f", "g"]));
+    segment_display_map.insert(9, HashSet::from(["a", "b", "c", "d", "f", "g"]));
 
     for line in text.lines() {
         let mut parts = line.split(" | ");
