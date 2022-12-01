ed268cce44bf8a178f841800657beef12661aa5d
Aaron McLeod <aaron.mcleod@wealthsimple.com>
2022-12-01 15:50:57 -0500

    playing with bitmasks, but probably dont need it

--- 8/src/main.rs
+++ 8/src/main.rs
@@ -1,9 +1,28 @@
 use std::io::Result;
 
+use bitflags::bitflags;
 use read_input::read_text;
+
+bitflags! {
+    struct Flags: u16 {
+        const A = 1;
+        const B = 2;
+        const C = 4;
+        const D = 8;
+        const E = 16;
+        const F = 32;
+        const G = 64;
+    }
+}
 
 fn main() -> Result<()> {
     let text = read_text("8/input.txt")?;
+
+    for line in text.lines() {
+        let mut parts = line.split(" | ");
+        let signal_patterns = parts.next().unwrap().split(" ").collect::<Vec<&str>>();
+        let output_value = parts.next().unwrap().split(" ").collect::<Vec<&str>>();
+    }
 
     Ok(())
 }
