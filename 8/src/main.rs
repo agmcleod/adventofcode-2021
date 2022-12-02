e97c6c424084189f9d8ff9bb7d93335a82f5b724
Aaron McLeod <aaron.mcleod@wealthsimple.com>
2022-12-02 16:00:32 -0500

    d8p1

--- 8/src/main.rs
+++ 8/src/main.rs
@@ -18,11 +18,32 @@ fn main() -> Result<()> {
     segment_display_map.insert(8, HashSet::from(["a", "b", "c", "d", "e", "f", "g"]));
     segment_display_map.insert(9, HashSet::from(["a", "b", "c", "d", "f", "g"]));
 
+    let mut p1_sum = 0;
+
     for line in text.lines() {
         let mut parts = line.split(" | ");
-        let signal_patterns = parts.next().unwrap().split(" ").collect::<Vec<&str>>();
-        let output_value = parts.next().unwrap().split(" ").collect::<Vec<&str>>();
+        let _signal_patterns = parts.next().unwrap().split(" ").collect::<Vec<&str>>();
+
+        let output_patterns = parts.next().unwrap().split(" ").collect::<Vec<&str>>();
+
+        for pattern in &output_patterns {
+            if pattern.len() == 2 {
+                // it's a 1
+                p1_sum += 1;
+            } else if pattern.len() == 3 {
+                // a 7
+                p1_sum += 1;
+            } else if pattern.len() == 4 {
+                // a 4
+                p1_sum += 1;
+            } else if pattern.len() == 7 {
+                // an 8
+                p1_sum += 1;
+            }
+        }
     }
+
+    println!("{}", p1_sum);
 
     Ok(())
 }
