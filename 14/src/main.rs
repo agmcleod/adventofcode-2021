39d4ca823c95667ff3b3155b1cddcc056e15f095
Aaron McLeod <aaron.g.mcleod@gmail.com>
2022-12-23 11:45:00 -0500

    day14p2, was able to get the answer, but i have an off by 1 count issue

--- 14/src/main.rs
+++ 14/src/main.rs
@@ -1,14 +1,26 @@
-use std::collections::{HashMap, LinkedList};
+use std::collections::HashMap;
 use std::io::Result;
 
 use read_input::read_text;
+
+fn insert_or_append_count(
+    map: &mut HashMap<(char, char), usize>,
+    key: &(char, char),
+    count: usize,
+) {
+    if map.contains_key(&key) {
+        *map.get_mut(&key).unwrap() += count;
+    } else {
+        map.insert(key.to_owned(), count);
+    }
+}
 
 fn main() -> Result<()> {
     let text = read_text("14/input.txt")?;
 
     let mut rules = HashMap::new();
 
-    let mut template = LinkedList::new();
+    let mut template = HashMap::new();
     let mut retrieved_template = false;
     for line in text.lines() {
         if retrieved_template {
@@ -18,60 +30,56 @@ fn main() -> Result<()> {
                 if pattern.len() != 2 {
                     panic!("Pattern is incorrect length {}", line);
                 }
-                let to_insert = iter.next().unwrap().to_owned();
+                let pattern: Vec<char> = pattern.chars().collect();
+                let to_insert = iter.next().unwrap();
+                if to_insert.len() != 1 {
+                    panic!("Transformed value is incorrect length {}", line);
+                }
+                let to_insert = to_insert.chars().next().unwrap();
 
-                rules.insert(pattern, to_insert);
+                rules.insert((pattern[0], pattern[1]), to_insert);
             }
         } else {
-            for letter in line.split("") {
-                if letter != "" {
-                    template.push_back(letter.to_string());
-                }
+            let letters = line.chars().collect::<Vec<char>>();
+            for pair in letters.windows(2) {
+                let key = (pair[0], pair[1]);
+                insert_or_append_count(&mut template, &key, 1);
             }
             retrieved_template = true;
         }
     }
 
     // or 10 for part 1
-    for _ in 0..10 {
-        let mut next_iteration = template.clone();
-        let mut insert_count = 0;
-        for i in 0..template.len() {
-            let iter = template.iter();
-            let mut iter = iter.skip(i);
-            let first = iter.next().unwrap();
-            let second = iter.next();
-            // second can be empty if we're at the end of the list
-            if second.is_none() {
-                break;
-            }
-            let second = second.unwrap();
-            if let Some(insert_value) = rules.get(&format!("{}{}", first, second)) {
-                let mut second_part = next_iteration.split_off(i + insert_count + 1);
-                insert_count += 1;
-                second_part.push_front(insert_value.to_owned());
-                next_iteration.append(&mut second_part);
-            } else {
-                panic!("No rule found for pair {}{}", first, second);
+    for _ in 0..40 {
+        let mut next_template = template.clone();
+        for (pair, count) in &template {
+            if rules.contains_key(pair) {
+                let next_count = next_template.get_mut(pair).unwrap();
+                *next_count -= *count;
+                if *next_count == 0 {
+                    next_template.remove(pair);
+                }
+                let transform = rules.get(pair).unwrap();
+                insert_or_append_count(&mut next_template, &(pair.0, *transform), *count);
+                insert_or_append_count(&mut next_template, &(*transform, pair.1), *count);
             }
         }
-
-        template = next_iteration;
+        template = next_template;
     }
 
-    let mut count = HashMap::new();
-    for el in &template {
-        if count.contains_key(el) {
-            *count.get_mut(el).unwrap() += 1;
+    let mut count_per_char = HashMap::new();
+    for ((l1, _l2), count) in &template {
+        if count_per_char.contains_key(l1) {
+            *count_per_char.get_mut(l1).unwrap() += *count;
         } else {
-            count.insert(el.to_owned(), 1);
+            count_per_char.insert(*l1, *count);
         }
     }
 
-    let mut min = std::i32::MAX;
-    let mut max = std::i32::MIN;
+    let mut min = std::usize::MAX;
+    let mut max = std::usize::MIN;
 
-    for (_el, count) in &count {
+    for (_el, count) in &count_per_char {
         min = min.min(*count);
         max = max.max(*count);
     }
