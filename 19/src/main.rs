ea04e9c0c5a52b2c7d4ca41a17a85c8117edec3a
Aaron McLeod <aaron.mcleod@wealthsimple.com>
2023-04-13 11:39:35 -0400

    starting

--- 19/src/main.rs
+++ 19/src/main.rs
@@ -5,5 +5,11 @@ use read_input::read_text;
 fn main() -> Result<()> {
     let text = read_text("19/input.txt")?;
 
+    for line in text.lines() {
+        if line.is_empty() {
+            continue;
+        }
+    }
+
     Ok(())
 }
