8f125a149710180b8c353dd3692ee0194ee1d34f
Aaron McLeod <aaron.mcleod@wealthsimple.com>
2023-05-01 10:42:36 -0400

    d20 parsing

--- 20/src/main.rs
+++ 20/src/main.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use std::io::Result;
 
 use read_input::read_text;
@@ -5,7 +6,20 @@ use read_input::read_text;
 fn main() -> Result<()> {
     let text = read_text("20/input.txt")?;
 
-    for line in text.lines() {}
+    let mut image_map = None;
+    let mut row = 0;
+    let mut grid = HashMap::new();
+
+    for line in text.lines() {
+        if image_map.is_none() {
+            image_map = Some(line.to_owned());
+        } else if !line.is_empty() {
+            for (col, ch) in line.chars().enumerate() {
+                grid.insert((col, row), ch.to_string());
+            }
+            row += 1;
+        }
+    }
 
     Ok(())
 }
