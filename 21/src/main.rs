d094989e11dd9c15ee5a020e61c57d2e2ca9bf93
Aaron McLeod <aaron.mcleod@wealthsimple.com>
2023-08-23 11:21:18 -0400

    d21 p2 recursion found the answer

--- 21/src/main.rs
+++ 21/src/main.rs
@@ -1,9 +1,9 @@
 use std::{collections::HashMap, fmt::Display};
 
-const PLAYER_1_POS: usize = 4;
-const PLAYER_2_POS: usize = 8;
+const PLAYER_1_POS: usize = 7;
+const PLAYER_2_POS: usize = 6;
 
-const PART_TWO_SCORE: usize = 10;
+const PART_TWO_SCORE: usize = 21;
 
 fn next_die(deterministic_die: &mut usize) {
     *deterministic_die = (*deterministic_die + 1) % 100;
@@ -27,45 +27,59 @@ fn deterministically_move_player(pos: usize, deterministic_die: &mut usize) -> u
     }
 }
 
-#[derive(Clone)]
-struct GameState {
+fn p2_play_game(
+    cache: &mut HashMap<(usize, usize, usize, usize), (usize, usize)>,
     p1_pos: usize,
     p2_pos: usize,
-    universe: usize,
     p1_score: usize,
     p2_score: usize,
-    p1_roll_value: usize,
-    p2_roll_value: usize,
-}
+    die_permutations: &Vec<usize>,
+) -> (usize, usize) {
+    if let Some((p1_unis, p2_unis)) = cache.get(&(p1_pos, p2_pos, p1_score, p2_score)) {
+        return (*p1_unis, *p2_unis);
+    }
 
-impl GameState {
-    fn new(p1_pos: usize, p2_pos: usize, p1_score: usize, p2_score: usize) -> Self {
-        GameState {
-            p1_pos,
-            p2_pos,
-            universe: 1,
-            p1_score,
-            p2_score,
-            p1_roll_value: 0,
-            p2_roll_value: 0,
+    let mut p1_universes_for_this_state = 0;
+    let mut p2_universes_for_this_state = 0;
+
+    for p1_roll in die_permutations {
+        for p2_roll in die_permutations {
+            let mut p1_pos = (p1_pos + *p1_roll) % 10;
+            if p1_pos == 0 {
+                p1_pos = 10;
+            }
+
+            let p1_score = p1_score + p1_pos;
+
+            let mut p2_pos = (p2_pos + *p2_roll) % 10;
+            if p2_pos == 0 {
+                p2_pos = 10;
+            }
+
+            let p2_score = p2_score + p2_pos;
+
+            if p1_score >= PART_TWO_SCORE {
+                p1_universes_for_this_state += 1;
+                break;
+            }
+
+            if p2_score >= PART_TWO_SCORE {
+                p2_universes_for_this_state += 1;
+            } else {
+                let (p1_sub_count, p2_sub_count) =
+                    p2_play_game(cache, p1_pos, p2_pos, p1_score, p2_score, die_permutations);
+                p1_universes_for_this_state += p1_sub_count;
+                p2_universes_for_this_state += p2_sub_count;
+            }
         }
     }
-}
 
-impl Display for GameState {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(
-            f,
-            "p1 pos {} roll {} score {}, p2 pos {} roll {} score {}, universe #{}",
-            self.p1_pos,
-            self.p1_roll_value,
-            self.p1_score,
-            self.p2_pos,
-            self.p2_roll_value,
-            self.p2_score,
-            self.universe
-        )
-    }
+    cache.insert(
+        (p1_pos, p2_pos, p1_score, p2_score),
+        (p1_universes_for_this_state, p2_universes_for_this_state),
+    );
+
+    (p1_universes_for_this_state, p2_universes_for_this_state)
 }
 
 fn main() {
@@ -109,70 +123,16 @@ fn main() {
         }
     }
 
-    let mut work = vec![GameState::new(PLAYER_1_POS, PLAYER_2_POS, 0, 0)];
-
-    let mut p1_universes = 0usize;
-    let mut p2_universes = 0usize;
-
     let mut part_two_cache = HashMap::new();
 
-    while let Some(state) = work.pop() {
-        if let Some((p1_unis, p2_unis)) =
-            part_two_cache.get(&(state.p1_pos, state.p2_pos, state.p1_score, state.p2_score))
-        {
-            p1_universes += p1_unis;
-            p2_universes += p2_unis;
-            continue;
-        }
-
-        let mut p1_universes_for_this_state = 0;
-        let mut p2_universes_for_this_state = 0;
-
-        for p1_roll in &die_permutations {
-            for p2_roll in &die_permutations {
-                let mut state = state.clone();
-                state.p1_roll_value = *p1_roll;
-                state.p1_pos = (state.p1_pos + *p1_roll) % 10;
-                if state.p1_pos == 0 {
-                    state.p1_pos = 10;
-                }
-
-                state.p1_score += state.p1_pos;
-
-                state.p2_roll_value = *p2_roll;
-                state.p2_pos = (state.p2_pos + *p2_roll) % 10;
-                if state.p2_pos == 0 {
-                    state.p2_pos = 10;
-                }
-
-                state.p2_score += state.p2_pos;
-
-                if state.p1_score >= PART_TWO_SCORE {
-                    p1_universes_for_this_state += state.universe;
-                    break;
-                }
-
-                if state.p2_score >= PART_TWO_SCORE {
-                    p2_universes_for_this_state += state.universe;
-                } else {
-                    work.push(state);
-                }
-            }
-        }
-
-        part_two_cache.insert(
-            (state.p1_pos, state.p2_pos, state.p1_score, state.p2_score),
-            (p1_universes_for_this_state, p2_universes_for_this_state),
-        );
-
-        p1_universes += p1_universes_for_this_state;
-        p2_universes += p2_universes_for_this_state;
-
-        // for state in &work {
-        //     println!("{}", state);
-        // }
-        // break;
-    }
+    let (p1_universes, p2_universes) = p2_play_game(
+        &mut part_two_cache,
+        PLAYER_1_POS,
+        PLAYER_2_POS,
+        0,
+        0,
+        &die_permutations,
+    );
 
     println!(
         "{} {}, p1 won? {}",
