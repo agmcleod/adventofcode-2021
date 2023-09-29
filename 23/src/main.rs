6710e827c0b6e31a523e0e28c100de3e423d8fe6
Aaron McLeod <aaron.mcleod@wealthsimple.com>
2023-09-29 16:10:31 -0400

    d23 setup to path to any hallway node math still off though

--- 23/src/main.rs
+++ 23/src/main.rs
@@ -4,7 +4,8 @@ use std::io::Result;
 
 use read_input::read_text;
 
-type Map = HashMap<(usize, usize), Tile>;
+type Coord = (usize, usize);
+type Map = HashMap<Coord, Tile>;
 
 #[derive(Clone, Eq, Hash, PartialEq)]
 enum Tile {
@@ -16,7 +17,7 @@ enum Tile {
 }
 
 impl Tile {
-    fn get_target_coords(&self) -> [(usize, usize); 2] {
+    fn get_target_coords(&self) -> [Coord; 2] {
         match *self {
             Tile::A => [(3, 2), (3, 3)],
             Tile::B => [(5, 2), (5, 3)],
@@ -86,15 +87,59 @@ impl State {
         }
     }
 
-    fn get_nonsolved_tiles(&self) -> impl Iterator<Item = (&(usize, usize), &Tile)> {
+    fn get_nonsolved_tiles(&self) -> impl Iterator<Item = (&Coord, &Tile)> {
         self.map
             .iter()
             .filter(|(_, tile)| **tile != Tile::Empty && !self.locations_solved.contains(*tile))
+    }
+
+    fn get_possible_hallway_tiles(&self, from: &Coord) -> Vec<Coord> {
+        let possible_hallway_tiles = [(1, 1), (2, 1), (4, 1), (6, 1), (8, 1), (10, 1), (11, 1)];
+
+        possible_hallway_tiles
+            .iter()
+            .filter(|coord| {
+                *self.map.get(coord).unwrap() == Tile::Empty && self.path_is_clear(from, coord)
+            })
+            .cloned()
+            .collect()
+    }
+
+    fn path_is_clear(&self, from: &Coord, to: &Coord) -> bool {
+        // since we're dealing with a fairly strict map, we can check y axis more deliberately
+        if from.1 != to.1 {
+            let mut y_sorted = [from, to];
+            y_sorted.sort_by(|a, b| a.1.cmp(&b.1));
+
+            for y in y_sorted[0].1..=y_sorted[1].1 {
+                // we use second coord's x axis, because that's the Y vertical we're checking
+                let coord = (y_sorted[1].0, y);
+                // if not the from spot, and a tile along the y axis is filled, return false
+                if coord != *from && *self.map.get(&coord).unwrap() != Tile::Empty {
+                    return false;
+                }
+            }
+        }
+
+        // now just check x axis
+
+        let min_x = from.0.min(to.0);
+        let max_x = from.0.max(to.0);
+
+        for x in min_x..=max_x {
+            let coord = (x, from.1.min(to.1));
+            if coord != *from && *self.map.get(&coord).unwrap() != Tile::Empty {
+                return false;
+            }
+        }
+
+        true
     }
 }
 
 impl Display for State {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        writeln!(f, "Energy: {}", self.energy)?;
         for r in 0..=self.height {
             for c in 0..=self.width {
                 if let Some(location) = self.map.get(&(c, r)) {
@@ -110,13 +155,14 @@ impl Display for State {
     }
 }
 
-fn manhatten_distance(coord1: &(usize, usize), coord2: &(usize, usize)) -> usize {
+fn manhatten_distance(coord1: &Coord, coord2: &Coord) -> usize {
     ((coord1.0 + coord1.1) as i32 - (coord2.0 + coord2.1) as i32).unsigned_abs() as usize
 }
 
 fn move_ampiphod_out_of_slot(
     mut state: State,
-    coord: &(usize, usize),
+    old_state: State,
+    coord: &Coord,
     tile: &Tile,
     number_of_moves: usize,
     total_energy_cost: &mut usize,
@@ -129,8 +175,8 @@ fn move_ampiphod_out_of_slot(
     let up_left_coord = (coord.0 - 1, coord.1 - (number_of_moves - 1));
     if *state.map.get(&up_left_coord).unwrap() == Tile::Empty {
         state.energy += tile.get_energy_cost() * number_of_moves;
-        state.map.insert((coord.0 - 1, coord.1 - 1), tile.clone());
-        *total_energy_cost = (*total_energy_cost).min(next_moves(state));
+        state.map.insert(up_left_coord, tile.clone());
+        *total_energy_cost = (*total_energy_cost).min(next_moves(old_state.clone(), state));
     }
 
     // move up and then right
@@ -138,28 +184,19 @@ fn move_ampiphod_out_of_slot(
     if *second_state.map.get(&up_right_coord).unwrap() == Tile::Empty {
         second_state.energy += tile.get_energy_cost() * number_of_moves;
         second_state.map.insert(up_right_coord, tile.clone());
-        *total_energy_cost = (*total_energy_cost).min(next_moves(second_state));
+        *total_energy_cost = (*total_energy_cost).min(next_moves(old_state, second_state));
     }
 }
 
-fn next_moves(state: State) -> usize {
-    // loop through each letter
-    // if letter is in desired spot
-    //   is there a letter below it not in desired spot?
-    //     move letter above it out of spot
-    //   else
-    //     leave as is
-    // elseif is letter in a slot
-    //   move letter out of slot (if space available)
-    // elseif is letter out of slot
-    //   can it move into slot?
-    //     move into slot
-    //   else
-    //     do nothing
+fn next_moves(old_state: State, state: State) -> usize {
+    // println!("old {}", old_state);
+    // println!("new {}", state);
 
     if state.locations_solved.len() == 4 {
         return state.energy;
     }
+
+    let old_state = state.clone();
 
     let mut energy_cost = usize::MAX;
 
@@ -171,39 +208,34 @@ fn next_moves(state: State) -> usize {
             if state.map.get(&target_coords[0]).unwrap() == tile {
                 let mut state = state.clone();
                 state.locations_solved.insert(tile.clone());
-                next_moves(state);
+                next_moves(old_state.clone(), state);
             }
             continue;
         } else if (*coord == target_coords[0] && state.map.get(&target_coords[1]).unwrap() != tile)
             || coord.1 == 2
+            || coord.1 == 3
         {
-            // letter is in desired top spot, but has a different letter below, or it is in top spot and needs to be elsewhere
-            move_ampiphod_out_of_slot(state.clone(), coord, tile, 2, &mut energy_cost);
-        } else if coord.1 == 3 {
-            if *state.map.get(&(coord.0, coord.1 - 1)).unwrap() == Tile::Empty {
-                move_ampiphod_out_of_slot(state.clone(), coord, tile, 3, &mut energy_cost);
+            // letter is right spot but needs to move out so the letter below it can move
+            // OR its in the wrong slot and needs to move out
+            for to_coord in &state.get_possible_hallway_tiles(coord) {
+                let mut state = state.clone();
+                state.map.insert(*coord, Tile::Empty);
+                state.map.insert(*to_coord, tile.clone());
+                state.energy += tile.get_energy_cost() * manhatten_distance(coord, to_coord);
+                energy_cost = energy_cost.min(next_moves(old_state.clone(), state));
             }
         } else if coord.1 == 1 {
             // y coord is 1, so is out of slot
-            let first_tile = state.map.get(&target_coords[1]).unwrap();
+            let first_tile = state.map.get(&target_coords[0]).unwrap();
             let second_tile = state.map.get(&target_coords[1]).unwrap();
             let can_move_into_bottom_spot =
                 *first_tile == Tile::Empty && *second_tile == Tile::Empty;
             let can_move_into_top_spot = *first_tile == Tile::Empty && *second_tile == *tile;
 
-            let mut ampiphods_in_the_way = false;
-            if can_move_into_bottom_spot || can_move_into_top_spot {
-                let min_x = coord.0.min(target_coords[0].0);
-                let max_x = coord.0.max(target_coords[0].0);
-
-                for x in (min_x + 1)..max_x {
-                    if *state.map.get(&(x, coord.1)).unwrap() != Tile::Empty {
-                        ampiphods_in_the_way = true;
-                    }
-                }
-            }
-
-            if !ampiphods_in_the_way {
+            // because of the first two boolean checks, we can just use the first target coord as the endpoint safely
+            if (can_move_into_bottom_spot || can_move_into_top_spot)
+                && state.path_is_clear(coord, &target_coords[0])
+            {
                 // both empty, can move into bottom spot
                 if can_move_into_bottom_spot {
                     let mut state = state.clone();
@@ -212,7 +244,7 @@ fn next_moves(state: State) -> usize {
                         tile.get_energy_cost() * manhatten_distance(coord, &target_coords[1]);
                     state.map.insert(target_coords[1], tile.clone());
                     // println!("Moving into bottom {} {}", tile, state);
-                    energy_cost = energy_cost.min(next_moves(state));
+                    energy_cost = energy_cost.min(next_moves(old_state.clone(), state));
                 } else if can_move_into_top_spot {
                     // top spot is empty, and bottom spot has the same tile value as our current letter
                     let mut state = state.clone();
@@ -222,7 +254,7 @@ fn next_moves(state: State) -> usize {
                     state.locations_solved.insert(tile.clone());
                     state.map.insert(target_coords[0], tile.clone());
                     // println!("Moving into top {} {}", tile, state);
-                    energy_cost = energy_cost.min(next_moves(state));
+                    energy_cost = energy_cost.min(next_moves(old_state.clone(), state));
                 }
             }
         }
@@ -252,7 +284,7 @@ fn main() -> Result<()> {
     }
 
     let state = State::new(map, width, height);
-    println!("{}", next_moves(state));
+    println!("{}", next_moves(state.clone(), state));
 
     Ok(())
 }
