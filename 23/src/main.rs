f4793636b26cba6dd849167a61dd0e65989d6920
Aaron McLeod <aaron.mcleod@wealthsimple.com>
2023-10-27 22:48:56 -0400

    d23 trying with real input

--- 23/src/main.rs
+++ 23/src/main.rs
@@ -4,9 +4,8 @@ use std::io::Result;
 
 use read_input::read_text;
 
-type Coord = (usize, usize);
+type Coord = (i32, i32);
 type Map = HashMap<Coord, Tile>;
-
 #[derive(Clone, Eq, Hash, PartialEq)]
 enum Tile {
     Empty,
@@ -27,7 +26,7 @@ impl Tile {
         }
     }
 
-    fn get_energy_cost(&self) -> usize {
+    fn get_energy_cost(&self) -> i32 {
         match *self {
             Self::A => 1,
             Self::B => 10,
@@ -69,6 +68,7 @@ impl Display for Tile {
 
 #[derive(Clone)]
 struct State {
+    old_state: Option<Box<State>>,
     energy: usize,
     map: Map,
     width: usize,
@@ -79,6 +79,7 @@ struct State {
 impl State {
     fn new(map: Map, width: usize, height: usize) -> Self {
         Self {
+            old_state: None,
             energy: 0,
             map,
             width,
@@ -142,7 +143,7 @@ impl Display for State {
         writeln!(f, "Energy: {}", self.energy)?;
         for r in 0..=self.height {
             for c in 0..=self.width {
-                if let Some(location) = self.map.get(&(c, r)) {
+                if let Some(location) = self.map.get(&(c as i32, r as i32)) {
                     write!(f, "{}", location)?;
                 } else {
                     write!(f, " ")?;
@@ -155,48 +156,23 @@ impl Display for State {
     }
 }
 
-fn manhatten_distance(coord1: &Coord, coord2: &Coord) -> usize {
-    ((coord1.0 + coord1.1) as i32 - (coord2.0 + coord2.1) as i32).unsigned_abs() as usize
+fn manhatten_distance(coord1: &Coord, coord2: &Coord) -> i32 {
+    (coord1.0 - coord2.0).unsigned_abs() as i32 + (coord1.1 - coord2.1).unsigned_abs() as i32
 }
 
-fn move_ampiphod_out_of_slot(
-    mut state: State,
-    old_state: State,
-    coord: &Coord,
-    tile: &Tile,
-    number_of_moves: usize,
-    total_energy_cost: &mut usize,
-) {
-    // move the current letter out
-    state.map.insert(*coord, Tile::Empty);
-    let mut second_state = state.clone();
-
-    // move up and then left
-    let up_left_coord = (coord.0 - 1, coord.1 - (number_of_moves - 1));
-    if *state.map.get(&up_left_coord).unwrap() == Tile::Empty {
-        state.energy += tile.get_energy_cost() * number_of_moves;
-        state.map.insert(up_left_coord, tile.clone());
-        *total_energy_cost = (*total_energy_cost).min(next_moves(old_state.clone(), state));
-    }
-
-    // move up and then right
-    let up_right_coord = (coord.0 + 1, coord.1 - (number_of_moves - 1));
-    if *second_state.map.get(&up_right_coord).unwrap() == Tile::Empty {
-        second_state.energy += tile.get_energy_cost() * number_of_moves;
-        second_state.map.insert(up_right_coord, tile.clone());
-        *total_energy_cost = (*total_energy_cost).min(next_moves(old_state, second_state));
-    }
-}
-
-fn next_moves(old_state: State, state: State) -> usize {
-    // println!("old {}", old_state);
-    // println!("new {}", state);
-
+fn next_moves(state: State) -> usize {
     if state.locations_solved.len() == 4 {
+        // if state.energy == 4281 {
+        //     let mut old_state = state.old_state.as_ref();
+        //     println!("printing solved");
+        //     println!("{}", state);
+        //     while old_state.is_some() {
+        //         println!("{}", old_state.unwrap());
+        //         old_state = old_state.as_ref().unwrap().old_state.as_ref();
+        //     }
+        // }
         return state.energy;
     }
-
-    let old_state = state.clone();
 
     let mut energy_cost = usize::MAX;
 
@@ -207,8 +183,9 @@ fn next_moves(old_state: State, state: State) -> usize {
             // if letter in top spot also matches, mark this one as complete
             if state.map.get(&target_coords[0]).unwrap() == tile {
                 let mut state = state.clone();
+                state.old_state = Some(Box::new(state.clone()));
                 state.locations_solved.insert(tile.clone());
-                next_moves(old_state.clone(), state);
+                energy_cost = energy_cost.min(next_moves(state));
             }
             continue;
         } else if (*coord == target_coords[0] && state.map.get(&target_coords[1]).unwrap() != tile)
@@ -219,10 +196,12 @@ fn next_moves(old_state: State, state: State) -> usize {
             // OR its in the wrong slot and needs to move out
             for to_coord in &state.get_possible_hallway_tiles(coord) {
                 let mut state = state.clone();
+                state.old_state = Some(Box::new(state.clone()));
                 state.map.insert(*coord, Tile::Empty);
                 state.map.insert(*to_coord, tile.clone());
-                state.energy += tile.get_energy_cost() * manhatten_distance(coord, to_coord);
-                energy_cost = energy_cost.min(next_moves(old_state.clone(), state));
+                state.energy +=
+                    (tile.get_energy_cost() * manhatten_distance(coord, to_coord)) as usize;
+                energy_cost = energy_cost.min(next_moves(state));
             }
         } else if coord.1 == 1 {
             // y coord is 1, so is out of slot
@@ -239,22 +218,26 @@ fn next_moves(old_state: State, state: State) -> usize {
                 // both empty, can move into bottom spot
                 if can_move_into_bottom_spot {
                     let mut state = state.clone();
+                    state.old_state = Some(Box::new(state.clone()));
                     state.map.insert(*coord, Tile::Empty);
-                    state.energy +=
-                        tile.get_energy_cost() * manhatten_distance(coord, &target_coords[1]);
+                    state.energy += (tile.get_energy_cost()
+                        * manhatten_distance(coord, &target_coords[1]))
+                        as usize;
                     state.map.insert(target_coords[1], tile.clone());
                     // println!("Moving into bottom {} {}", tile, state);
-                    energy_cost = energy_cost.min(next_moves(old_state.clone(), state));
+                    energy_cost = energy_cost.min(next_moves(state));
                 } else if can_move_into_top_spot {
                     // top spot is empty, and bottom spot has the same tile value as our current letter
                     let mut state = state.clone();
+                    state.old_state = Some(Box::new(state.clone()));
                     state.map.insert(*coord, Tile::Empty);
-                    state.energy +=
-                        tile.get_energy_cost() * manhatten_distance(coord, &target_coords[0]);
+                    state.energy += (tile.get_energy_cost()
+                        * manhatten_distance(coord, &target_coords[0]))
+                        as usize;
                     state.locations_solved.insert(tile.clone());
                     state.map.insert(target_coords[0], tile.clone());
                     // println!("Moving into top {} {}", tile, state);
-                    energy_cost = energy_cost.min(next_moves(old_state.clone(), state));
+                    energy_cost = energy_cost.min(next_moves(state));
                 }
             }
         }
@@ -277,14 +260,14 @@ fn main() -> Result<()> {
                 continue;
             }
 
-            map.insert((c, r), ch.into());
+            map.insert((c as i32, r as i32), ch.into());
             width = width.max(c);
         }
         height += 1;
     }
 
     let state = State::new(map, width, height);
-    println!("{}", next_moves(state.clone(), state));
+    println!("{}", next_moves(state));
 
     Ok(())
 }
@@ -298,5 +281,6 @@ mod tests {
         assert_eq!(manhatten_distance(&(1, 1), &(2, 2)), 2);
         assert_eq!(manhatten_distance(&(1, 1), &(6, 2)), 6);
         assert_eq!(manhatten_distance(&(6, 2), &(1, 1)), 6);
+        assert_eq!(manhatten_distance(&(10, 1), &(9, 2)), 2);
     }
 }
