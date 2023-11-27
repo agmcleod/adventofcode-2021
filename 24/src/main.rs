f5a4306e51e55f1e4ff350f1b27c43af5b0264d7
Aaron McLeod <aaron.mcleod@wealthsimple.com>
2023-11-27 17:43:00 -0500

    d24 sorted by sourcing some answers i dont fully get it, but i kinda understand

--- 24/src/main.rs
+++ 24/src/main.rs
@@ -1,64 +1,95 @@
-fn decrement_number(number_digits: &mut Vec<i32>) {
-    if number_digits.len() != 14 {
-        panic!(
-            "Invalid length of digits, should be 14. was {}",
-            number_digits.len()
-        );
+use std::collections::HashSet;
+
+type BadState = HashSet<(usize, i32)>;
+
+// borrowed solution from: https://github.com/VSZM/Advent_Of_Code/blob/master/2021/AOC2021/Day24.cs, as hints on reddit werent the most clear as to the patterns to look at
+fn run_sub_program(
+    inputs: &Vec<i32>,
+    bad_states: &mut BadState,
+    mut model_number: usize,
+    z: i32,
+    depth: usize,
+    divs: &Vec<i32>,
+    adds: &Vec<i32>,
+    second_adds: &Vec<i32>,
+) -> Option<usize> {
+    if bad_states.contains(&(depth, z)) || depth == 14 {
+        return None;
     }
-    let mut index_to_decrement = 13;
-    loop {
-        let digit_value = number_digits.get_mut(index_to_decrement as usize).unwrap();
-        *digit_value -= 1;
-        if *digit_value == 0 {
-            *digit_value = 9;
-            index_to_decrement -= 1;
-            if index_to_decrement < 0 {
-                break;
-            }
-        } else {
-            break;
+
+    // this is to shift the number left, so were adding to the next place in the number
+    model_number *= 10;
+
+    for w in inputs {
+        let mut x = z;
+        x %= 26;
+        let mut z = z / divs[depth];
+        x += adds[depth];
+        x = if x == *w { 0 } else { 1 };
+        let mut y = 25;
+        y *= x;
+        y += 1;
+        z *= y;
+        y = 0;
+        y += w + second_adds[depth];
+        y *= x;
+        z += y;
+
+        // reached success state
+        if z == 0 && depth == 13 {
+            return Some(model_number + *w as usize);
+        }
+
+        let return_val = run_sub_program(
+            inputs,
+            bad_states,
+            model_number + *w as usize,
+            z,
+            depth + 1,
+            divs,
+            adds,
+            second_adds,
+        );
+        if return_val.is_some() {
+            return return_val;
         }
     }
+
+    // because of the way z accumulates in the program, we know that at this depth level it simply doesnt work
+    // this acts as an operational cache
+    bad_states.insert((depth, z));
+
+    None
 }
 
 fn main() {
-    let mut inputs = vec![9; 14];
-
     let divs = vec![1, 1, 1, 26, 26, 1, 1, 1, 26, 26, 26, 1, 26, 26];
     let adds = vec![12, 12, 15, -8, -4, 15, 14, 14, -13, -3, -7, 10, -6, -8];
     let second_adds = vec![1, 1, 16, 5, 9, 3, 2, 15, 5, 11, 7, 1, 10, 3];
 
-    loop {
-        let mut x;
-        let mut y;
-        let mut z = 0;
+    let mut bad_states: BadState = HashSet::new();
 
-        for (i, inp) in inputs.iter().enumerate() {
-            let w = *inp;
+    let mn = run_sub_program(
+        &(1..=9).rev().collect(),
+        &mut bad_states,
+        0,
+        0,
+        0,
+        &divs,
+        &adds,
+        &second_adds,
+    );
+    println!("{:?}", mn);
 
-            x = z;
-            x %= 26;
-            z /= divs[i];
-            x += adds[i];
-            x = if x == w { 0 } else { 1 };
-            y = 25;
-            y *= x;
-            y += 1;
-            z *= y;
-            y = 0;
-            y += w + second_adds[i];
-            y *= x;
-            z += y;
-        }
-
-        if z == 0 {
-            println!(
-                "{}",
-                inputs.iter().map(|n| n.to_string()).collect::<String>()
-            );
-            break;
-        } else {
-            decrement_number(&mut inputs);
-        }
-    }
+    let mn = run_sub_program(
+        &(1..=9).collect(),
+        &mut bad_states,
+        0,
+        0,
+        0,
+        &divs,
+        &adds,
+        &second_adds,
+    );
+    println!("{:?}", mn);
 }
